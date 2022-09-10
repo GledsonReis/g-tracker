d2b58bdc6efb90786aab419a6815c27d46b055de
Gledson Maelson Passos Reis <gledson.reis@maida.health>
2022-09-10 16:00:12 -0300

    showing the task's project

--- src/components/TaskVue.vue
+++ src/components/TaskVue.vue
@@ -1,8 +1,11 @@
 <template>
     <BoxVue>
         <div class="columns">
-            <div class="column is-7">
-                {{ task.description || 'Tarefa sem descrição'}}
+            <div class="column is-4">
+                {{ task.description || 'Tarefa sem descrição' }}
+            </div> 
+            <div class="column is-3">
+                {{ task.project?.name || 'N/D' }}
             </div>
             <div class="column">    
                 <StopwatchVue :timeInSeconds="task.timeInSeconds"/>
